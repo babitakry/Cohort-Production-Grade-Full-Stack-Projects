9de6be3b1589bae36bc7a910e3725267bab13e64
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-06-04 21:58:52 +0530

    Add  TypeScript configuration for Next.js project

--- NextJS/Next-Js-Assignment/todo/lib/db.ts
+++ NextJS/Next-Js-Assignment/todo/lib/db.ts
@@ -4,8 +4,9 @@ const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) {
         return;
     }
-    const connection = await mongoose.connect(process.env.MONGO_URI!);
-    console.log(`MongoDB connected: ${connection.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI!);
+    // console.log(`MongoDB: ${conn.connection}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
 }
 
 export default connectDB;
