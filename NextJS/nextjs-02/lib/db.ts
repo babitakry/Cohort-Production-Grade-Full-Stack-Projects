d8d828d9e153c10d8b92dfd154e4417eac4b121d
Babita Kumari <babitakumari.ky3007@gmail.com>
2026-06-06 00:19:09 +0530

    feat: integrate @neondatabase/serverless with Prisma and update build script to run prisma generate

--- NextJS/nextjs-02/lib/db.ts
+++ NextJS/nextjs-02/lib/db.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon"; // from install @prisma/adapter-neon package
-
-
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
@@ -12,10 +11,10 @@ function createPrismaClient() {
     if (!url) {
         throw new Error("DATABASE_URL is not set.");
     }
-    const adapter = new PrismaNeon({ connectionString: url });
+    const pool = new Pool({ connectionString: url });
+    const adapter = new PrismaNeon(pool as any);
     return new PrismaClient({ adapter });
 }
-
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
